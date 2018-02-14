bae9efc1e6accaac3f88f8321b01d97032f146df
wangchengyu1997 <wangchengyu@wangchengyudeMacBook-Pro.local>
2018-02-14 00:51:49 -0800

    handled auth

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    mongoURI:'mongodb://<dbuser>:<dbpassword>@ds231568.mlab.com:31568/storybook-chengyu',
+    googleClientID:'519923311944-3ddas7tcvg6n5f1c51d13dq1030ee0m6.apps.googleusercontent.com',
+    googleClienSecret:'BrUm7N1oDh5JkdRAkwoE6yea'
+}
+
